72fb3b63f6b7842bfc3ef48929464c7aae733e43
arfian pradana <arfianpradana07@gmail.com>
2022-11-08 18:40:46 +0700

    ok

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html>
       <head>
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="theme-color" content="#1E293B" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="note app by arfian" />
         <title>TYPNOTE</title>
