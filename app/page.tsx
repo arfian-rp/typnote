de8fc0623af7bf9079477fbedb7c0c3de015b589
arfian pradana <arfianpradana07@gmail.com>
2022-11-08 19:05:04 +0700

    ok

--- app/page.tsx
+++ app/page.tsx
@@ -23,14 +23,9 @@ export default function Page() {
   return (
     <div className="mx-[0.7rem] md:mx-[1rem] lg:mx-[10rem] text-secondary font-mono">
       <div className="flex justify-between items-center px-10 lg:px-40 font-semibold text-center my-5">
-        <div
-          onClick={() => {
-            window.location.href = "/";
-          }}
-          className="text-4xl hover:cursor-pointer hover:text-secondary-hover"
-        >
+        <a href="/" className="text-4xl hover:cursor-pointer hover:text-secondary-hover">
           TypNote
-        </div>
+        </a>
       </div>
       <hr />
       <div>
